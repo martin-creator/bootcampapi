738eeb9c2edd854981c5df62e146f3d430a54c47
martin-creator <martinlubowa@outlook.com>
2023-10-12 22:54:39 +0300

    Added logic to handle unhandled promises

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-// Route files
-const bootcampRoutes = require('./routes/bootcampRoutes');
+
+const app = express();
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-const app = express();
+// Connect to database
+connectDB();
+
+// Route files
+const bootcampRoutes = require('./routes/bootcampRoutes');
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -22,7 +27,15 @@ app.use('/api/v1/bootcamps', bootcampRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhadled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+    console.log(`Error: ${error.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+}
 );
