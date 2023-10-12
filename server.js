edbf9b988e90bc1e6cd664207305652d11e45d9c
martin-creator <martinlubowa@outlook.com>
2023-10-12 23:05:42 +0300

    Added data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 
@@ -29,12 +30,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
 
 // Handle unhadled promise rejections
 process.on('unhandledRejection', (error, promise) => {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red);
     // Close server & exit process
     server.close(() => process.exit(1));
 }
