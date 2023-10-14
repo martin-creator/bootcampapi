3f787bc2cc9a9f69e5e35ba8b7d9c8d504cb3b32
martin-creator <martinlubowa@outlook.com>
2023-10-14 21:59:59 +0300

    Added validation error

--- middleware/error.js
+++ middleware/error.js
@@ -11,13 +11,19 @@ const errorHandler = (err, req, res,next) => {
     // Mongoose bad ObjectId
     if(err.name === 'CastError'){
         const message = `Resource not found with id of ${err.value}`;
-        err = new ErrorResponse(message, 404);
+        error = new ErrorResponse(message, 404);
     }
 
     // Mongoose duplicate key
     if(err.code === 11000){
         const message = 'Duplicate field value entered';
-        err = new ErrorResponse(message, 400);
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
