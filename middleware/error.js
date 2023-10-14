8ba1d19acd45e47b613a676e40582392fbf7eacd
martin-creator <martinlubowa@outlook.com>
2023-10-14 21:54:23 +0300

    Added logic to handle duplicates

--- middleware/error.js
+++ middleware/error.js
@@ -14,6 +14,12 @@ const errorHandler = (err, req, res,next) => {
         err = new ErrorResponse(message, 404);
     }
 
+    // Mongoose duplicate key
+    if(err.code === 11000){
+        const message = 'Duplicate field value entered';
+        err = new ErrorResponse(message, 400);
+    }
+
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error'
