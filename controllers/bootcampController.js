915669a02a0d00b0ab3ed35c7756080e83468ace
martin-creator <martinlubowa@outlook.com>
2023-10-14 20:36:37 +0300

    Added logic to update single bootcamp

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -3,15 +3,32 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-const getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps', hello: req.hello });
+const getBootcamps =  async (req, res, next) => {
+    try{
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+    }catch(err){
+        res.status(400).json({ success: false });
+    }
+   
 }
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-const getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+const getBootcamp =  async (req, res, next) => {
+    try{
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if(!bootcamp){
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    }catch(err){
+        res.status(400).json({ success: false });
+    }
 }
 
 // @desc    Create new bootcamp
@@ -33,8 +50,17 @@ const createBootcamp = async (req, res, next) => {
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-const updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+const updateBootcamp =  async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if(!bootcamp){
+        return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
 }
 
 // @desc    Delete bootcamp
