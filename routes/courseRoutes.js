3a84d7d1aa4c95ad8f90441ac987c66700d2bf96
martin-creator <martinlubowa@outlook.com>
2023-10-19 23:32:58 +0300

    Protected  course routes

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -13,16 +13,18 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const {protectRoute} = require('../middleware/auth');
+
 router.route('/')
 .get(advancedResults(Course, {
     path: 'bootcamp',
     select: 'name description'
 }), getCourses)
-.post(addCourse);
+.post(protectRoute, addCourse);
 
 router.route('/:id')
 .get(getCourse)
-.put(updateCourse)
-.delete(deleteCourse);
+.put(protectRoute, updateCourse)
+.delete(protectRoute, deleteCourse);
 
 module.exports = router;
