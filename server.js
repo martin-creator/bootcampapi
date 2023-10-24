bb0c8664673a0a1e9558f4913b3d42383a70e08a
martin-creator <martinlubowa@outlook.com>
2023-10-25 01:52:41 +0300

    added cost

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xssClean = require('xss-clean');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const hpp = require('hpp');
 const fileUpload = require('express-fileupload');
 const connectDB = require('./config/db');
@@ -63,6 +64,9 @@ app.use(limiter);
 
 // Prevent http param pollution
 app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
