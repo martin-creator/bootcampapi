67eface21770801eedb411cf1615e3d27ad6234b
martin-creator <martinlubowa@outlook.com>
2023-10-25 01:51:16 +0300

    Added rate limiting

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const fileUpload = require('express-fileupload');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
@@ -50,6 +52,17 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xssClean());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
